a9a3631cb3b41c77de5bec86b51f65246b158c2d
eladman7 <eladman77@gmail.com>
2020-07-12 20:34:09 +0300

    map reduce for filtering only ngrams which contain hyprynm pair

--- src/main/java/FilterHypernymBiarcsMR.java
+++ src/main/java/FilterHypernymBiarcsMR.java
@@ -25,21 +25,41 @@ public class FilterHypernymBiarcsMR {
 
         @Override
         public void map(LongWritable lineId, Text gram, Context context) throws IOException, InterruptedException {
-            String[] splittedNGram = gram.toString().split("\\s+");
-            if (splittedNGram.length == 3 &&
-                    Boolean.parseBoolean(splittedNGram[2])) {// annotated pair
-                String key = splittedNGram[0] + "\t" + splittedNGram[1];
-                context.write(new Text(key), new Text("true"));
+            String[] splittedNGram = gram.toString().trim().split("\\s+");
+            if (splittedNGram.length == 3) {// annotated pair
+                if (Boolean.parseBoolean(splittedNGram[2])) {
+                    String key = splittedNGram[0] + "\t" + splittedNGram[1];
+                    context.write(new Text(key + "1"), new Text("true"));
+                }
             } else {
                 List<String> allNounPairsByNgram = createAllNounPairsByNgram(gram);
                 for (String pair : allNounPairsByNgram) {
-                    context.write(new Text(pair), gram);
+                    context.write(new Text(pair + "2"), gram);
                 }
             }
         }
 
         private List<String> createAllNounPairsByNgram(Text gram) {
-            return new ArrayList<>();
+            List<String> result = new ArrayList<>();
+            String[] splitNGram = gram.toString().trim().split("\\s+");
+            List<String> onlyNounsLst = new ArrayList<>();
+            for (int i = 0; i < splitNGram.length - 1; i++) {
+                if (LingusticUtils.isNoun(splitNGram[i + 1])) {
+                    onlyNounsLst.add(splitNGram[i + 1]);
+                }
+            }
+            int half = (onlyNounsLst.size() % 2 == 0) ? onlyNounsLst.size() / 2 : (onlyNounsLst.size() / 2) + 1;
+            for (int i = 0; i < half; i++) {
+                for (int j = i + 1; j < onlyNounsLst.size(); j++) {
+                    result.add(extractWord(onlyNounsLst.get(i)) + "\t" + extractWord(onlyNounsLst.get(j)));
+                    result.add(extractWord(onlyNounsLst.get(j)) + "\t" + extractWord(onlyNounsLst.get(i)));
+                }
+            }
+            return result;
+        }
+
+        private String extractWord(String wordDescription) {
+            return wordDescription.substring(0, wordDescription.indexOf("/"));
         }
     }
 
@@ -51,22 +71,46 @@ public class FilterHypernymBiarcsMR {
     }
 
     public static class ReducerClass extends Reducer<Text, Text, Text, Text> {
+        public String currentKey;
+
         public void setup(Context context) {
+            currentKey = "";
         }
+
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            values.forEach(System.out::println);
+            Text firstVal = values.iterator().next();
+            String extractTag = extractTag(key);
+            if (firstVal.toString().contains("true") && getTag(key).equals("1")) {
+                currentKey = extractTag;
+            } else {
+                if (extractTag.equals(currentKey)) {
+                    context.write(new Text(extractTag), firstVal);
+                    for (Text value : values) {
+                        context.write(new Text(extractTag), value);
+                    }
+                }
+            }
         }
+
 
         public void cleanup(Context context) {
         }
     }
 
+    public static String getTag(Text key) {
+        return String.valueOf(key.toString().charAt(key.toString().length() - 1));
+    }
+
+    public static String extractTag(Text key) {
+        return key.toString().substring(0, key.toString().length() - 1);
+    }
+
     public static class PartitionerClass extends Partitioner<Text, Text> {
         @Override
         public int getPartition(Text key, Text value, int numPartitions) {
-            return (key.hashCode() & Integer.MAX_VALUE) % numPartitions;
+            return (extractTag(key).hashCode() & Integer.MAX_VALUE) % numPartitions;
         }
     }
 
