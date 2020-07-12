c51d09266c0595afa90473a1cd2de9807fdabe98
eladman7 <eladman77@gmail.com>
2020-07-12 18:27:52 +0300

    yalla

--- src/main/java/FilterHypernymBiarcsMR.java
+++ src/main/java/FilterHypernymBiarcsMR.java
@@ -1,3 +1,4 @@
+import com.google.inject.internal.util.$SourceProvider;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -12,44 +13,60 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilterHypernymBiarcsMR {
-    public static class MapperClass extends Mapper<LongWritable, Text, Text, LongWritable> {
+    public static class MapperClass extends Mapper<LongWritable, Text, Text, Text> {
 
         @Override
         protected void setup(Context context) {
 
         }
 
-
         @Override
         public void map(LongWritable lineId, Text gram, Context context) throws IOException, InterruptedException {
+            String[] splittedNGram = gram.toString().split("\\s+");
+            if (splittedNGram.length == 3 &&
+                    Boolean.parseBoolean(splittedNGram[2])) {// annotated pair
+                String key = splittedNGram[0] + "\t" + splittedNGram[1];
+                context.write(new Text(key), new Text("true"));
+            } else {
+                List<String> allNounPairsByNgram = createAllNounPairsByNgram(gram);
+                for (String pair : allNounPairsByNgram) {
+                    context.write(new Text(pair), gram);
+                }
+            }
+        }
 
+        private List<String> createAllNounPairsByNgram(Text gram) {
+            return new ArrayList<>();
         }
     }
 
-    public static class CombinerClass extends Reducer<Text, LongWritable, Text, LongWritable> {
+    public static class CombinerClass extends Reducer<Text, Text, Text, Text> {
         @Override
-        public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
         }
     }
 
-    public static class ReducerClass extends Reducer<Text, LongWritable, Text, LongWritable> {
+    public static class ReducerClass extends Reducer<Text, Text, Text, Text> {
         public void setup(Context context) {
         }
 
         @Override
-        public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            values.forEach(System.out::println);
         }
 
         public void cleanup(Context context) {
         }
     }
 
-    public static class PartitionerClass extends Partitioner<Text, LongWritable> {
+    public static class PartitionerClass extends Partitioner<Text, Text> {
         @Override
-        public int getPartition(Text key, LongWritable value, int numPartitions) {
+        public int getPartition(Text key, Text value, int numPartitions) {
             return (key.hashCode() & Integer.MAX_VALUE) % numPartitions;
         }
     }
@@ -72,7 +89,6 @@ public class FilterHypernymBiarcsMR {
 
         Path oneGram = new Path(args[0]);
         Path twoGram = new Path(args[1]);
-//        Path decs = new Path(args[2]);
         Path outputPath = new Path(args[2]);
         MultipleInputs.addInputPath(job, oneGram, TextInputFormat.class, MapperClass.class);
         MultipleInputs.addInputPath(job, twoGram, TextInputFormat.class, MapperClass.class);
