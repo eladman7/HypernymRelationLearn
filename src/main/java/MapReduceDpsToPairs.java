10722c21484f25e1c6da77d90d90a2a518f6337c
barhadad <barhadad9@gmail.com>
2020-07-13 19:45:27 +0300

    Second map work, without reduce.

--- src/main/java/MapReduceDpsToPairs.java
+++ src/main/java/MapReduceDpsToPairs.java
@@ -6,16 +6,15 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-public class FilterHypernymBiarcsMR {
+public class MapReduceDpsToPairs {
+
     public static class MapperClass extends Mapper<LongWritable, Text, Text, Text> {
 
         @Override
@@ -28,40 +27,72 @@ public class FilterHypernymBiarcsMR {
 
         public void map(LongWritable lineId, Text gram, Context context) throws IOException, InterruptedException {
             String[] splittedNGram = gram.toString().trim().split("\\s+");
-            if (splittedNGram.length == 3) {        // annotated pair
-                if (Boolean.parseBoolean(splittedNGram[2])) {
-                    String key = splittedNGram[0] + "\t" + splittedNGram[1];
-                    context.write(new Text(key + "1"), new Text("true"));
-                }
-            } else {
-                List<String> allNounPairsByNgram = createAllNounPairsByNgram(gram);
-                for (String pair : allNounPairsByNgram) {
-                    context.write(new Text(pair + "2"), gram);
-                }
+            StringBuilder ngram = new StringBuilder("");
+            String key = splittedNGram[0] + " " + splittedNGram[1];
+            for (int i = 3; i< splittedNGram.length; i++) {
+                ngram.append(splittedNGram[i]);
+                ngram.append('\t');
             }
+
+            String dp = extractDps(key, ngram.toString());
+            context.write(new Text(dp), new Text(key));
+
         }
 
-        private List<String> createAllNounPairsByNgram(Text gram) {
-            List<String> result = new ArrayList<>();
-            String[] splitNGram = gram.toString().trim().split("\\s+");
-            List<String> onlyNounsLst = new ArrayList<>();
-            for (int i = 0; i < splitNGram.length - 1; i++) {
-                if (LingusticUtils.isNoun(splitNGram[i + 1])) {
-                    onlyNounsLst.add(splitNGram[i + 1]);
+
+        private String extractDps(String key, String ngram) {
+            HashMap<String, String> graph = new HashMap<>();
+            String[] splittedNGram = ngram.trim().split("\\s+");
+
+            for(int index = 0; index < splittedNGram.length; index++) {
+                String word = splittedNGram[index];   // Take only the noun
+                graph.put(getWord(word), splittedNGram[Integer.valueOf(get_next(word))]);
+                if(index != 0) {
+                    graph.put(word.split("/")[0], splittedNGram[Integer.valueOf(get_next(word)) - 1]);
                 }
             }
-            int half = (onlyNounsLst.size() % 2 == 0) ? onlyNounsLst.size() / 2 : (onlyNounsLst.size() / 2) + 1;
-            for (int i = 0; i < half; i++) {
-                for (int j = i + 1; j < onlyNounsLst.size(); j++) {
-                    result.add(extractWord(onlyNounsLst.get(i)) + "\t" + extractWord(onlyNounsLst.get(j)));
-                    result.add(extractWord(onlyNounsLst.get(j)) + "\t" + extractWord(onlyNounsLst.get(i)));
+
+            String key_first = key.split("\\s+")[0]; // From
+            String key_sec = key.split("\\s+")[1];  // To
+            String next = graph.get(key_first);
+            StringBuilder dp = new StringBuilder(getFullGram(splittedNGram, key_first) + " ");
+            StringBuilder dp_res = new StringBuilder();
+            // TODO: 13/07/2020 What if there are no path?
+            while (!next.split("/")[0].equals(key_sec)) {
+                dp.append(next).append(" ");
+                next = graph.get(getWord(next)); // Move to the next node.
+            }
+            dp.append(next);
+            System.out.println(dp.toString());
+            String[] splitted = dp.toString().split("\\s+");
+            for(String str : splitted) {
+                /* TODO: 13/07/2020 should be X?!? */
+                if(LingusticUtils.isNoun(str) && (str.contains(key_first) || str.contains(key_sec))) {
+                    dp_res.append(str.replace(str.split("/")[0], "X")).append(" ");
+                } else {
+                    dp_res.append(str).append(" ");
                 }
             }
-            return result;
+            return dp_res.toString();
         }
 
-        private String extractWord(String wordDescription) {
-            return wordDescription.substring(0, wordDescription.indexOf("/"));
+        private String get_next(String gram) {
+            String[] splittedNGram = gram.trim().split("/");
+            return splittedNGram[splittedNGram.length - 1];
+
+        }
+
+        private String getWord(String gram) {
+            return gram.split("/")[0];
+        }
+
+        private String getFullGram(String [] grams, String gram){
+            for(String word : grams) {
+                if (word.contains(gram)) {
+                    return word;
+                }
+            }
+            return "";
         }
     }
 
@@ -82,18 +113,7 @@ public class FilterHypernymBiarcsMR {
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            Text firstVal = values.iterator().next();
-            String extractTag = extractTag(key);
-            if (firstVal.toString().contains("true") && getTag(key).equals("1")) {
-                currentKey = extractTag;
-            } else {
-                if (extractTag.equals(currentKey)) {
-                    context.write(new Text(extractTag), firstVal);
-                    for (Text value : values) {
-                        context.write(new Text(extractTag), value);
-                    }
-                }
-            }
+            System.out.println("in reducer");
         }
 
 
@@ -119,8 +139,8 @@ public class FilterHypernymBiarcsMR {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = new Job(conf, "filter only biarcs with hypernym");
-        job.setJarByClass(FilterHypernymBiarcsMR.class);
+        Job job = new Job(conf, "Make dps to pairs table");
+        job.setJarByClass(MapReduceDpsToPairs.class);
         job.setMapperClass(MapperClass.class);
         job.setPartitionerClass(PartitionerClass.class);
 //      job.setCombinerClass(ReducerClass.class);
@@ -131,15 +151,9 @@ public class FilterHypernymBiarcsMR {
         job.setOutputValueClass(LongWritable.class);
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-
-        Path oneGram = new Path(args[0]);
-        Path twoGram = new Path(args[1]);
-
-//      Path decs = new Path(args[2]);
-        Path outputPath = new Path(args[2]);
-        MultipleInputs.addInputPath(job, oneGram, TextInputFormat.class, MapperClass.class);
-        MultipleInputs.addInputPath(job, twoGram, TextInputFormat.class, MapperClass.class);
-
+        Path input_path = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+        FileInputFormat.addInputPath(job, input_path);
         FileOutputFormat.setOutputPath(job, outputPath);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
